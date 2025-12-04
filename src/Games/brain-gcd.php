f1855414eda47c093cceb4cedc843c2a278eb55d
Aleksey <alekseyarkt@gmail.com>
2025-12-04 23:10:01 +0300

    0.1.31

--- src/Games/brain-gcd.php
+++ src/Games/brain-gcd.php
@@ -4,9 +4,9 @@ function brainGcdFunc(): array
 {
     $numbA = rand(1, 50);
     $numbB = rand(1, 50);
-    $result = $numbA;
+    $result = $numbB;
     $question = "{$numbA} {$numbB}";
-
+/*
     do {
         if ($numbB === 0) {
             $result = $numbA;
@@ -17,6 +17,17 @@ function brainGcdFunc(): array
             $numbB = $tmp % $numbB;
         }
     } while ($numbB >= 0);
+    */
+
+    while ($numbB != 0) {
+        $tmp = $numbA;
+        $numbA = $numbB;
+        $numbB = $tmp % $numbB;
+        if ($numbB === 0) {
+            $result = $numbA;
+            break;
+        }
+    }
 
     $array = [];
     $array['question'] = $question;
